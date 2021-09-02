cd7f4fcc08630c2beba3f90a64675b634636bc40
Nilgün Belma Bugüner <nilgun@php.net>
2021-09-02 17:13:58 +0300

    There shouldn't be a line like this.

--- scripts/revcheck.php
+++ scripts/revcheck.php
@@ -889,7 +889,7 @@ HTML;
               }
               print <<<HTML
  <tr class=bggray>
-  <td class="c"><a href="https://github.com/php/doc-en/blob/{$en->hash}/$key">$en->name</a></td>
+  <td class="c">$en->name</td>
   <td class="c">$en->size</td>
  </tr>
 HTML;
