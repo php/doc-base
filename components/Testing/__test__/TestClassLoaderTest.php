11d433dc5f205d3bfc0ee17aed81b7ba4faca78d
Eugène d'Augier <eugene.augier@gmail.com>
2022-05-01 14:46:32 +0200

    add file headers

--- components/Testing/__test__/TestClassLoaderTest.php
+++ components/Testing/__test__/TestClassLoaderTest.php
@@ -1,5 +1,23 @@
 <?php
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 8                                                      |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2011 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:    Eugène d'Augier <eugene-augier@gmail.com>                |
+  +----------------------------------------------------------------------+
 
+  $Id$
+*/
 use PHPDoc\Internal\Testing\Assert;
 use PHPDoc\Internal\Testing\Loader\TestClassLoader;
 
