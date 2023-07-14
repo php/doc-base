7b74f59752a33d5182d668393e32fced7e971b9f
André L F S Bacci <ae@php.net>
2023-07-14 14:11:34 -0300

    Rename for future use.

--- scripts/translation/lib/RevcheckRun.php
+++ scripts/translation/lib/RevcheckRun.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/require.php';
 /**
  * Calculate translation sync/diff status from two directories
  */
-class RevcheckInfo
+class RevcheckRun
 {
     public RevcheckFileList $sourceFiles;
     public RevcheckFileList $targetFiles;
