ed29a72a5a2d7e95ac3d56378c3ab4fc755e1e78
André L F S Bacci <ae@php.net>
2023-12-14 10:48:39 -0300

    License code to PHP License

--- scripts/translation/lib/CacheFile.php
+++ scripts/translation/lib/CacheFile.php
@@ -1,10 +1,23 @@
 <?php
+/**
+ *  +----------------------------------------------------------------------+
+ *  | Copyright (c) 1997-2023 The PHP Group                                |
+ *  +----------------------------------------------------------------------+
+ *  | This source file is subject to version 3.01 of the PHP license,      |
+ *  | that is bundled with this package in the file LICENSE, and is        |
+ *  | available through the world-wide-web at the following url:           |
+ *  | https://www.php.net/license/3_01.txt.                                |
+ *  | If you did not receive a copy of the PHP license and are unable to   |
+ *  | obtain it through the world-wide-web, please send a note to          |
+ *  | license@php.net, so we can mail you a copy immediately.              |
+ *  +----------------------------------------------------------------------+
+ *  | Authors:     André L F S Bacci <ae php.net>                          |
+ *  +----------------------------------------------------------------------+
+ *  | Description: Class to handle data persistence.                       |
+ *  +----------------------------------------------------------------------+
+ */
 
 require_once __DIR__ . '/all.php';
-
-/**
- * Class to handle data persistence
- */
 
 class CacheFile
 {
